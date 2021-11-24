1fd39935e33d5337e795aa2d297843d20c4b590f
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-24 16:26:12 +0300

    Update 0-promise.js

--- 0x00-ES6_basic/0-promise.js
+++ 0x00-ES6_basic/0-promise.js
@@ -1,3 +1,14 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => resolve);
+	// Only make Promise
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+      /* eslint-enable */
+  });
 }
+
+export default getResponseFromAPI;
