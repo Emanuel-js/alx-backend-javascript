cb18b9d865bbba17dfd476aeab8b7b3e1e498eb4
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-30 21:35:10 +0300

    Create  9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const grs = new Map();
+
+  grs.set('Apples', 10);
+  grs.set('Tomatoes', 10);
+  grs.set('Pasta', 1);
+  grs.set('Rice', 1);
+  grs.set('Banana', 5);
+  return grs;
+}
