ac7da2db75b9703e51f7b4b6e2bd9721b05ad8f3
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-30 20:26:20 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
 export default function getListStudentIds(list) {
-
-    if (Array.isArray(list))
-        return list.map(l => l.id);
+  if (!Array.isArray(list)) {
     return [];
   }
+  return list.map((item) => {
+    return item.id;
+  });
+}
