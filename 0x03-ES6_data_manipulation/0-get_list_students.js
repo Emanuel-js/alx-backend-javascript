d76916cd2a053e5d6c43cfa5c38bb33e9bc75a63
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-30 20:11:25 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,6 @@
-const studentsList = [
-  {
-    id: 1,
-    firstName: 'Guillame',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Colombia',
-  },
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  },
-];
 export default function getListStudents() {
-  return studentsList;
-}
+  return [  
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+]};
