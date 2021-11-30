3e3164afe5bb04124902eb411df0c65a271cdc57
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-30 19:37:31 +0300

    return array of object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,7 @@
 export default function getListStudents() {
-  student = [
-    {
-      id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstName: "James",
-      location: "Columbia",
-    },
-    {
-      id: 3,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
+  return [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
-
-  return student;
 }
